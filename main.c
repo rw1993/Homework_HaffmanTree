4318509a9c4bc6daf158c64570f205c8097381c5
rw13//gitcafe <470177412@qq.com>
2013-11-17 23:11:56 +0800

    can creat huffman tree

--- main.c
+++ main.c
@@ -1,14 +1,20 @@
 #include <stdio.h>
 #include<stdlib.h>
 #include<string.h>
+#define MAX 1000
 typedef struct{
 int weight;
 int lchild;
 int rchild;
 int parent;
-int tag;
+char tag;
 char c;
 }htnode;
+typedef struct{
+	char c;
+	char bits[MAX];
+	int len;
+}htcode;
 void InitHT(int*w,char*c,int m,int len,htnode*ht)
 {
       for(int i=0;i<m;i++)
@@ -25,20 +31,47 @@ void InitHT(int*w,char*c,int m,int len,htnode*ht)
       }
 
 };
-void SelectMin(htnode*ht,int n,int p1,int p2){};
+void SelectMin(htnode*ht,int n,int &p1,int &p2)
+{
+	for(int i=0;i<n;i++)
+	{
+		if(ht[i].parent==-1)
+		{
+			p1=i;
+			break;
+		}
+	}
+	for(int j=0;j<n;j++)
+	{
+		if((ht[j].weight<ht[p1].weight)&&(ht[j].parent==-1)) p1=j;
+        }
+	ht[p1].parent=n;
+	ht[n].lchild=p1;
+	ht[p1].tag='0';
+	for(int i=0;i<n;i++)
+	{
+                if(ht[i].parent==-1)
+		{
+			p2=i;
+			break;
+		}
+	}
+	for(int j=0;j<n;j++)
+	{
+		if((ht[j].weight<ht[p1].weight)&&(ht[j].parent==-1)) p2=j;
+	}
+	ht[p2].parent=n;
+	ht[p2].tag='1';
+	ht[n].rchild=p2;
+	ht[n].weight=ht[p1].weight+ht[p2].weight;
+};
 void creatHT(int m,int len,char*c,int*w,htnode*ht)
 {
 	int i,p1,p2;
 	InitHT(w,c, m, len,ht);
-	for(i=len;i<=m;i++)
+	for(i=len;i<m;i++)
 	{
-		SelectMin(ht,i-1,p1,p2);
-		ht[p1].parent=i;
-		ht[p2].parent=i;
-		ht[i].lchild=p1;
-		ht[i].rchild=p2;
-		ht[p1].tag=0;
-		ht[p2].tag=1;
+		SelectMin(ht,i,p1,p2);
 	}
 };
 int getCW(char*tmp,char*c,int* weight)
@@ -69,28 +102,53 @@ len++;
 
 return len;
 };
+void gethtcode(int n,htcode*hc,htnode*ht)
+{
+   for(int i=0;i<n;i++)
+   {
+	  /* htnode tmp;
+	   tmp=ht[i];
+	   hc[i].len=0;
+	   while(tmp.parent!=-1)
+	   {
+		   hc[i].c=tmp.c;
+		   hc[i].bits[i]=tmp.tag;
+		   hc[i].len++;
+		   tmp=ht[tmp.parent];
+           }*/
+	  // printf("%d,%d,%c\n",i,ht[i].parent,ht[i].c);
+   }
+  
+};
 int main()//main fanction,read the files,use other function
 {
-FILE*f;
-f=fopen("test.txt","r");
-fseek(f,0,SEEK_END);
-int file_size=ftell(f);
-//printf("%d",file_size);
-char*tmp;
-fseek(f,0,SEEK_SET);
-tmp=(char*)malloc(file_size*sizeof(char));
-fread(tmp,file_size,sizeof(char),f);
-//printf("%s\n",tmp);
-int*weight=(int*)malloc(file_size*sizeof(int));
-char*c=(char*)malloc(file_size*sizeof(char));
-int len=getCW(tmp,c,weight);//get char and weight
-// printf("%d\n",len);
-/* for(int i=0;i<len;i++)
+  FILE*f;
+  f=fopen("test.txt","r");
+  fseek(f,0,SEEK_END);
+  int file_size=ftell(f);
+  //printf("%d",file_size);
+  char*tmp;
+  fseek(f,0,SEEK_SET);
+  tmp=(char*)malloc(file_size*sizeof(char));
+  fread(tmp,file_size,sizeof(char),f);
+  //printf("%s\n",tmp);
+  int*weight=(int*)malloc(file_size*sizeof(int));
+  char*c=(char*)malloc(file_size*sizeof(char));
+  int len=getCW(tmp,c,weight);//get char and weight
+  // printf("%d\n",len);
+  /* for(int i=0;i<len;i++)
  {
       printf("%c\n",c[i]);
  }*/
-int m=2*file_size-1;
-htnode*ht=(htnode*)malloc(m*sizeof(htnode));//begin to build the tree
-creatHT(m,strlen(tmp),c,weight,ht);
+  int m=2*len-1;
+  htnode*ht=(htnode*)malloc(m*sizeof(htnode));//begin to build the tree
+  creatHT(m,len,c,weight,ht);//creat the tree
+  //printf("%d\n",ht[m-1].parent);
+  htcode*hc=(htcode*)malloc(len*sizeof(htcode));//get diy_acssi
+  gethtcode(len,hc,ht);//getthecode
+  for(int i=0;i<m;i++)
+  {
+	  printf("%d,%d,%d,%d\n",i,ht[i].parent,ht[i].tag,ht[i].weight);
+  }
 return 0;
 }
